bd12080ff41207fc1da5907631e5d1a031349eaa
goatney420 <84412113+goatney420@users.noreply.github.com>
2023-04-10 19:01:45 -0400

    Add files via upload

---
+++ src/pages/MyMentors.js
@@ -0,0 +1,12 @@
+import Card from '../components/Card'
+import '../index.css'
+
+export default function MyMentors() {
+    return (
+        <div class="right-container">
+            <Card/>
+            <Card/>
+            <Card/>
+        </div> 
+    )
+}
