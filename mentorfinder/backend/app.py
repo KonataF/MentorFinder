5a5785ba5846d4838bc8890928c4d96095dfc342
Ali Shehbaz <alishahbaz072@gmail.com>
2023-04-19 23:05:16 -0400

    test: app connection

--- mentorfinder/backend/app.py
+++ mentorfinder/backend/app.py
@@ -1,9 +1,26 @@
 import time
-from flask import Flask
+from flask import Flask, jsonify
+from database import Database
+from dotenv import load_dotenv
+import os
+import json
 
 app = Flask(__name__)
 
 
-@app.route('/time')
+@app.route("/admin", methods=['get'])
+def admin():
+    return {'time': "Hello from admin panel"}
+
+
+@ app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+
+if __name__ == "__main__":
+    load_dotenv()
+    app.secret_key = os.environ.get("app_secret_key")
+    app.config['SESSION_TYPE'] = 'filesystem'
+    Database.initialize()
+    app.run(debug=True)
---
+++ mentorfinder/backend/database.py
@@ -0,0 +1,24 @@
+import pymongo
+from dotenv import load_dotenv
+import os
+
+
+class Database:
+    @classmethod
+    def initialize(cls):
+        load_dotenv()
+        uname = os.environ.get("uname")
+        db_key = os.environ.get("key")
+        connectionURL = f"mongodb+srv://{uname}:{db_key}@mentorfinder.kjb2und.mongodb.net/?retryWrites=true&w=majority"
+        print(connectionURL)
+        client = pymongo.MongoClient(connectionURL)
+        print(client.server_info())
+        cls.database = client.get_database("MentorFinderDB")
+
+    @classmethod
+    def get_collections(cls):
+        return cls.database.list_collection_names()
+
+    @classmethod
+    def get_collection(cls, collection):
+        return cls.database[collection]
