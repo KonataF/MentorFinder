42386a8052054750fd2763380dc0879431eb3ceb
Ali Shehbaz <alishahbaz072@gmail.com>
2023-04-19 21:24:55 -0400

    remove: everything

--- Event.py
+++
@@ -1,8 +0,0 @@
-class Event:
-    def __init__(self, id, time, date, location, guestList=[], description=""):
-        self.id = id
-        self.time = time
-        self.date = date
-        self.location = location
-        self.guestList = guestList
-        self.description = description
--- README.md
+++
@@ -1,3 +0,0 @@
-# MentorFinder
-
-> in order to run the project, replace the username and password in connection url
