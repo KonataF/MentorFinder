3be2090e6831e9bda0439be4ff9a2b9ee224b312
goatney420 <84412113+goatney420@users.noreply.github.com>
2023-04-27 17:12:36 -0400

    changed code how fun

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,35 @@
 import './App.css';
 import Navbar from './Navbar';
 import React, { useState } from "react";
-import Dashboard from './pages/Dashboard';
 import FindMentor from './pages/FindMentor';
 import MyMentors from './pages/MyMentors';
+//import Home from './pages/Home';
 import { Login } from "./pages/Login";
 import { Register } from "./pages/Register";
 import { Route, Routes } from "react-router-dom"
+import DashboardLeftColumn from "./src/pages/DashboardLeftColumn"
+import DashboardMiddleColumn from "./src/pages/DashboardMiddleColumn"
+import DashboardRightColumn from "./src/pages/DashboardRightColumn"
 
 export function App() {
-  // const [currentForm, setCurrentForm] = useState('login');
+  const [currentForm, setCurrentForm] = useState('login');
 
-  // const toggleForm = (formName) => {
-  //   setCurrentForm(formName);
-  // }
+  const toggleForm = (formName) => {
+    setCurrentForm(formName);
+  }
+
+  const DashboardLayout = ()=>{
+    return(
+      <div>
+        <Navbar/>
+        <div style={{display:"flex"}}>
+          <DashboardLeftColumn/>
+          <DashboardMidddleColumn/>
+          <DashboardRightColumn/>
+        </div>
+      </div>
+    )
+  }
 
   return (
     <>
@@ -21,42 +37,43 @@ export function App() {
       <Navbar />
       <div className="container">
         <Routes>
+          {/* need to figure out a way to re-rounte to dashboard page when you click Site Name */}
+          {/* <Route path="/" element={<Login />} /> */}
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/findmentor" element={<FindMentor />} />
           <Route path="/mymentors" element={<MyMentors />} />
-          <Route path="/login" element={<Login />}/>
         </Routes>
       </div>
 
-      {/* <div className="App">
+      <div className="App">
       {
         currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <Register onFormSwitch={toggleForm} />
       }
-      </div> */}
-      {/* <div className="menu-container">
+      </div>
+      <div className="menu-container">
         <div className="menu_trigger">
 
-      </div> */}
-        {/* <div className="educationDropdown">
+        </div>
+        <div className="educationDropdown">
           <h3> Unused <br/><span></span></h3>
           <ul>
             <DropdownItem/>
           </ul>
         </div>
-      </div> */}
+      </div>
 
     
     </>
   )
 }
-// function DropdownItem(props) {
-//   return(
-//     //**Not sure what is going on here */
-//     // <li className = 'dropdownItem'></li>
-//     // <img></img>
-//     //   <a> [props.text] </a>
-//     <p>sample text</p>
-//   );
-// }
+function DropdownItem(props) {
+  return(
+    //**Not sure what is going on here */
+    // <li className = 'dropdownItem'></li>
+    // <img></img>
+    //   <a> [props.text] </a>
+    <p>sample text</p>
+  );
+}
 
 export default App;
--- my-app/src/pages/Dashboard.js
+++ my-app/src/pages/Dashboard.js
@@ -1,5 +1,45 @@
-
+import '../index.css'
 
 export default function Dashboard() {
-    return <h1>Dashboard</h1>
+    return(
+        // <div>
+        //    <h2>Featured</h2>
+        // </div>
+        <div>
+          <div className="rightCollumn">
+            <div className="container">
+                <div className="communityHubCard">
+                    <div className="community">
+                        <h3>Community Hubs</h3> 
+                        searchbar
+                        <button>Follow</button>
+                    </div>
+                <div className="communityHubFollowingCard">
+                    <h3>Following</h3>
+                    <button entire button on click></button>
+                </div>
+                <div classname="calendarCard">
+                    <h3>Calendar</h3>
+                    make a calendar
+                </div>
+                </div>
+            </div>
+            </div>      
+
+            <div className='leftColumn'>
+            <div className="container">
+                <div className="featured"></div>
+                    <br></br>
+                    <h3>Featured</h3>
+                    <img src= "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fclipground.com%2Fimages%2Fblack-rectangle-border-clipart-1.jpg&f=1&nofb=1&ipt=b076bb5ecce44680fa592a3ea5b3a93b4520e81398d6b97507e85f194796bef7&ipo=images"></img>
+            </div>
+             </div>
+        </div>
+        
+
+        
+    )
+
 }
+  
+    
