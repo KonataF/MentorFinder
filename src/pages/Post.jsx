cd4a9a945c803f352442c57f560d9fe6cafcc813
KonataF <125525114+KonataF@users.noreply.github.com>
2023-05-04 19:23:24 -0400

    Add files via upload

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,19 +1,31 @@
 import "./CommunityHubPosts.scss"
+import "Upvote.js"
+import "Downvote.js"
+import {getCurrentDate} from "./GetCurrentDate"
+console.log(getCurrentDate())
 
 
 const Post = ({post}) => {
 	return (
 	<div className="communityHubPost">
 		<div className="user">
-			<div className="userInfo"></div>
-			<img src={postMessage.img} alt=""/>
-			<div className="details">
-				<Link to={`/profile/${post.user}`} style={{textDecoration:"none", color:"inherit"}}>
-					<span>{post.name}</span>
-				</Link>
+			<div classname="container">
+				<div className="userInfo"></div>
+					<img src={postMessage.profilePic} alt=""/>
+					<div className="details">
+						<Link to={`/profile/${post.user}`} style={{textDecoration:"none", color:"inherit"}}>
+							<span classname="name"> {post.name}</span>
+					</Link>
+					<span className="date">  </span>
+				</div>
 			</div>
-			<div className="content"></div>
-			<div className="content"></div>
+		<div className="content">
+			<p>{post.desc}</p>
+			<ing src={post.img} alt="" />"
+		</div>
+		<div className="info"></div>
+		<Upvote/>
+		<Downvote/>
 		</div>
 	</div>
 	)
