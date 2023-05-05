8f46afeac491e09090f6199e950ed9bc2e74c0f9
KonataF <125525114+KonataF@users.noreply.github.com>
2023-05-05 18:02:55 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ export function App() {
         <Navbar/>
         <div style={{display:"flex"}}>
           <DashboardLeftColumn/>
-          <DashboardMidddleColumn/>
+          <DashboardMiddleColumn/>
           <DashboardRightColumn/>
         </div>
       </div>
