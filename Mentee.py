91c289e9bb29eb17093e7aceb43797200f5f1db9
KonataF <125525114+KonataF@users.noreply.github.com>
2023-05-05 21:32:01 -0400

    Delete Mentee.py

--- Mentee.py
+++
@@ -1,9 +0,0 @@
-from User import User
-
-
-class Mentee(User):
-    def __init__(self, id, name, email, username, password, dob, occupation, education,
-                 experience, bio,  profilePic='', mentorList=[]) -> None:
-        super().__init__(id, name, email, username, password, dob, occupation, education,
-                         experience, bio, profilePic, type="mentee")
-        self.mentorList = mentorList
