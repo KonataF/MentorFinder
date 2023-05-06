e601ad17d72c3e39a55adfc9ff72c591c8d006d8
KonataF <125525114+KonataF@users.noreply.github.com>
2023-05-05 21:27:31 -0400

    Add files via upload

---
+++ mentorfinder/src/pages/DashboardRightColumn.jsx
@@ -0,0 +1,28 @@
+import "./DashboardRightColumn.scss"
+
+const DashboardRightColumn = () => {
+    return (
+        <div className="rightCollumn">
+            <div className="container">
+                <div className="communityHubCard">
+                    <div className="community">
+                        <h3>Community Hubs</h3> 
+                        searchbar
+                        <button>Follow</button>
+                    </div>
+                <div className="communityHubFollowingCard">
+                    <h3>Following</h3>
+                    <button entire button on click></button>
+                </div>
+                <div classname="calendarCard">
+                    <h3>Calendar</h3>
+                    make a calendar
+                </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DashboardRightColumn
+
